466086ae4bbda95cc16d455f551ffb1b19a93e49
kaeli <puiesabu@gmail.com>
2014-03-28 18:14:45 +0800

    Implement Methods Inheritance

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-module.exports = Class = function(argv) {
+module.exports = Class = function(argv, parent) {
   var constructor = argv["initialize"] || function() {};
+
+  if (typeof parent === "function") {
+    constructor.prototype = new parent();
+    constructor.prototype.constructor = constructor;
+  }
+
   for (var key in argv) {
     if (key !== "initialize") {
       constructor.prototype[key] = argv[key];
     }
   }
+
   return constructor;
 };
